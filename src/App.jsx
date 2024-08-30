24516cea09a070f73301a2762427f055678b8d2c
Dipesh <dipeshchourasiya12@gmail.com>
2024-08-30 18:49:39 +0530

    Navbar and Footer components styled no.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 
 // import './App.css'
 
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
+
 function App() {
 
   return (
     <div>
-      <h1 className='bg-blue-900 h-screen text-white flex items-center justify-center'>Hello World</h1>
+      {/* <h1 className='bg-blue-900 h-screen text-white flex items-center justify-center'>Hello World</h1> */}
+      <Navbar />
+      <Home/>
+      <Footer />
     </div>
   )
 }
 
-export default App
+export default App;
